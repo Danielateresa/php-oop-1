532ed3989933b82359c755f17da80be81954fb84
Danielateresa <dany.t_87@hotmail.it>
2022-12-09 12:00:26 +0100

    Final commit

--- index.php
+++ index.php
@@ -21,31 +21,83 @@ require __DIR__ . '/db.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
 </head>
 
-<body>
+<style>
+main {
+    height: calc(100% - 69px);
+    background: color: #212529;
+}
 
-    <h1>Movies Info</h1>
+.card {
+    min-height: 280px;
+    background-color: #171718;
+}
 
-    <ul>
-        <?php foreach ($Movies as $Movie) :?>
-        <li>
-            <h3>Title: <?php echo $Movie->name ?></h3>
-            <p>Genre:
-                <span>
-                    <?php foreach ($Movie->genres as $singleGenre) :?>
-                    <?php echo $singleGenre->diffGenre ?>
-                    <?php endforeach; ?>
-                </span>
-            </p>
+.container {
+    max-width: 900px;
+}
 
-            <p>Description: <?php echo $Movie->description ?></p>
-            <p>Duration: <?php echo $Movie->duration ?></p>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+header {
+    background-color: #212529;
+}
 
+h3 {
+    color: #d7d7d7;
+}
+
+p {
+    color: gray;
+}
+</style>
+
+<body class="vh-100">
+
+    <div id="header" class="text-center text-black py-2">
+        <h1 class="pb-3">Movies Info</h1>
+    </div>
+    <!-- /#header -->
+
+    <main class="bg-black">
+        <div class="container h-100">
+            <div class="row g-3 h-100">
+                <?php foreach ($Movies as $Movie) :?>
+                <div class="col-4 my-5">
+                    <div class="card px-2 py-4">
+                        <h3><?php echo $Movie->name ?></h3>
+                        <p>Genre:
+                            <span>
+                                <?php foreach ($Movie->genres as $singleGenre) :?>
+                                <?php echo $singleGenre->diffGenre ?>
+                                <?php endforeach; ?>
+                            </span>
+                        </p>
+
+                        <p>Description: <?php echo $Movie->description ?></p>
+                        <p>Duration: <?php echo $Movie->duration ?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+    </main>
+    <!-- /#main -->
 
 </body>
+
+<!-- Bootstrap JavaScript Libraries -->
+<script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+</script>
 
 </html>
